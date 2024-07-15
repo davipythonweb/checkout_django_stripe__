cd52f8f702e8c07f946fb883c7dd70c776c8a5a9
davipythonweb <galaxys3tab32@gmail.com>
2024-07-15 02:01:54 -0300

    create models `product`

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+    # para mostrar o preço formatado com duas casas decimais
+    def exibe_price(self):
+        return "{:.2f}".format(self.price)
+
