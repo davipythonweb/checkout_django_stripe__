3ab3c5850a6d9c9cadf87c1a14e244cd5527808f
davipythonweb <galaxys3tab32@gmail.com>
2024-07-15 03:00:45 -0300

    create views de pagamento e urls

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('create-checkout-session/<int:id>', views.create_checkout_session, name='create_checkout_session'),
+
+    path('sucesso/', views.sucesso, name='sucesso'),
+    
+    path('erro/', views.erro, name='erro'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,94 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+import stripe
+from django.conf import settings
+
+from .models import Product
+from django.http import JsonResponse, HttpResponse
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
 
 def home(request):
+    print(stripe.api_key)
     return render(request, 'home.html')
 
 
+
+def create_checkout_session(request, id):
+    product = Product.objects.get(id = id)
+    YOUR_DOMAIN = "http://127.0.0.1:8000/"
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data':{
+                    'currency': 'BRL',
+                    'unit_amount' : int(product.price),
+                    'product_data': {
+                        'name': product.name
+                    }
+                },
+                'quantity': 1,
+            },
+        ],
+        payment_method_types=[
+            'card',
+            'boleto',
+        ],
+        metadata={
+            'id_product': product.id,
+        },
+        mode='payment',
+        success_url=YOUR_DOMAIN + '/sucesso',
+        cancel_url=YOUR_DOMAIN + '/erro',
+    )
+    return JsonResponse({'id': checkout_session.id})
+
+
+def sucesso(request):
+    return HttpResponse('Sucesso!')
+
+
+def erro(request):
+    return HttpResponse('Erro!')
+
+
+# se fosse em produção
+"""
+
+def create_checkout_session(request, id):
+    product = Product.objects.get(id = id)
+    if settings.DEBUG:
+        YOUR_DOMAIN = "http://127.0.0.1:8000/"
+    else:
+        NOVO_DOMAIN = 'http://seudominio.com.br'
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data':{
+                    'currency': 'BRL',
+                    'unit_amount' : int(product.price),
+                    'product_data': {
+                        'name': product.name
+                    }
+                },
+                'quantity': 1,
+            },
+        ],
+        payment_method_types=[
+            'card',
+            'boleto',
+        ],
+        metadata={
+            'id_product': product.id,
+        },
+        mode='payment',
+        success_url=YOUR_DOMAIN + '/sucesso',
+        cancel_url=YOUR_DOMAIN + '/erro',
+    )
+    return JsonResponse({'id': checkout_session.id})
+
+    
+"""
